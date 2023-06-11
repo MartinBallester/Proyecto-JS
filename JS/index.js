b66974f3db9bd43fd5fc40d241cb14a89046e3b4
martin <ballester.1990@gmail.com>
2023-06-11 17:31:38 -0300

    trabajo 2

--- JS/index.js
+++ JS/index.js
@@ -5,9 +5,6 @@ const email = document.querySelector('#email');
 const form = document.querySelector('#form');
 const parrafo = document.querySelector('#alerta')
 const imputs = document.querySelectorAll('#form imput')
-
-
-
 let descuentoEstudiante = 0.8;
 let descuentoTrainee = 0.5;
 let descuentoJunior = 0.15;
@@ -18,33 +15,60 @@ const botonResumen = document.querySelector('#botonResumen');
 const botonBorrar = document.querySelector('#borrar');
 const totalCompra = document.querySelector('#totalCompra');
 
-function totalPagar (){
-    let totalValor = parseInt(cantidad.value) * valorTicket;
+// function validar(evento){
+//     evento.preventDefault();
+// }
 
-    if(categoria.value == 1){
-        totalValor = totalValor - (totalValor * descuentoEstudiante);
+function totalPagar (evento){
+    evento.preventDefault();
+    let totalValor = parseInt(cantidad.value) * valorTicket;
+    var emailValido = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var nombreValido = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+    var cantidadValida= /^\d{1,3}$/;
+    
+    if(!nombre.value.match(nombreValido) ){
+        swal("Error", "Por favor ingrese un nombre valido", "error");
+    }
+    else if(!apellido.value.match(nombreValido)){
+        swal("Error", "Por favor ingrese un apellido valido", "error");
+    }
+    else if(!email.value.match(emailValido)){      
+        swal("Error", "Por favor ingrese un email valido", "error");
+    }
+    else if(!cantidad.value.match(cantidadValida)){
+        swal("Error", "Por favor ingrese una cantidad valida", "error");
+    }
+    else if(categoria.value == 0){
+        swal("Error", "Por favor elija una categoria", "error");
+    }  
+    else if(categoria.value == 1){
+        totalValor = totalValor;
     }
     else if(categoria.value == 2){
-        totalValor = totalValor - (totalValor * descuentoTrainee);
+        totalValor = totalValor - (totalValor * descuentoEstudiante);
     }
     else if(categoria.value == 3){
+        totalValor = totalValor - (totalValor * descuentoTrainee);
+    }
+    else if(categoria.value == 4){
         totalValor = totalValor - (totalValor * descuentoJunior);
     }
-    
+   
     totalCompra.innerHTML = `Total a pagar : $ ${totalValor}`;
     
 }
 
-botonResumen.addEventListener('click', totalPagar => {
-
-    totalPagar.preventDefault();
-})
-
+botonResumen.addEventListener('click', totalPagar)
 botonBorrar.addEventListener('click', () => {
-    totalCompra.innerHTML = 'Total a pagar : '
-    cantidad.value = null;
+    totalCompra.innerHTML = 'Total a pagar : $'
+    cantidad.value = "";
+    nombre.value = "";
+    apellido.value = "";
+    email.value = "";
+    categoria.value = "0";
     
 })
+
 
 
 
